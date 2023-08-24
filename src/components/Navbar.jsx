73c0c47c3f114442713960975023573e73edb2e5
zLeobii <69085621+zLeobii@users.noreply.github.com>
2023-08-24 23:42:03 +0200

    Day 1 Part 2

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,18 +4,18 @@ import Linkedin from "../assets/linkedinblack.svg";
 
 const Navbar = () => {
     return (
-        <div className="bg-main3 flex justify-center items-center w-[100dvw] gap-[400px] h-[119px] pt-[10px] pb-[10px]">
+        <div className="bg-main3 flex sm:flex-row flex-col justify-center items-center w-[100dvw] h-[136px] sm:h-[119px] 2xl:gap-[400px] xl:gap-[350px] lg:gap-[150px] sm:gap-[50px] gap-[15px] pt-[10px] pb-[10px]">
             <div className="flex justify-center items-center gap-[0.6rem] height-[5.1rem]">
-                <img src={Logo1} alt="logo" className="w-[69px] h-[69px] object-contain"/>
+                <img src={Logo1} alt="logo" className="lg:w-[69px] lg:h-[69px] md:w-[55px] md:h-[55px] sm:w-[50px] h-[50px] object-contain"/>
                 <h3 className="font-montserrat text-center font-semibold text-black2 text-2xl">summereth</h3>
             </div>
             <div className="flex h-[78px] justify-center items-center gap-[25px]">
-                <h5 className="font-montserrat text-center font-semibold text-black1 text-lg">Our System</h5>
-                <h5 className="font-montserrat text-center font-semibold text-black1 text-lg">Case Studies</h5>
-                <div className="flex justify-center items-center gap-[20px] pt-[15px] pb-[15px] h-[50px] w-[138px] rounded-xl border-solid border-main1 border-[2px]">
-                    <h5 className="font-montserrat text-center font-semibold text-main1 text-lg">Book a Call</h5>
+                <h5 className="font-montserrat text-center font-semibold text-black1 lg:text-lg md:text-base text-[15px]">Our System</h5>
+                <h5 className="font-montserrat text-center font-semibold text-black1 lg:text-lg md:text-base text-[15px]">Case Studies</h5>
+                <div className="flex justify-center items-center pt-[15px] pb-[15px] lg:h-[50px] lg:w-[138px] h-[35px] w-[99px]  rounded-xl border-solid border-main1 border-[2px]">
+                    <h5 className="font-montserrat text-center font-semibold text-main1 lg:text-lg text-sm">Book a Call</h5>
                 </div>
-                <div className="flex justify-center items-center gap-[30px]">
+                <div className="lg:flex hidden justify-center items-center gap-[30px]">
                     <img src={Twitter} alt="twitter" className="w-[45px] h-[45px] object-contain"/>
                     <img src={Linkedin} alt="linkedin" className="w-[45px] h-[45px] object-contain"/>
                 </div>
